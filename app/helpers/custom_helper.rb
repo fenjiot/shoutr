4da3f26df51fdd8b2be2550c15cadf595a884a75
fenjiot <fenjiot@gmail.com>
2014-10-16 16:38:52 -0400

    Autolink existing usernames and hashtags

---
+++ app/helpers/custom_helper.rb
@@ -0,0 +1,33 @@
+module CustomHelper
+  def autolink body
+    body_link_usernames = link_username(body)
+    link_hashtag(body_link_usernames)
+  end
+
+  def link_username(body)
+    username_pattern = /@\w+/i
+
+    body.gsub(username_pattern) do |username|
+      username_without_at = username.downcase[1..-1]
+      bolded ="<strong>#{username}</strong>".html_safe
+      if User.find_by(username: username_without_at)
+        link_to bolded, user_path(username_without_at)
+      else
+        bolded
+      end
+    end
+  end
+
+  def link_hashtag(body)
+    hashtag_pattern = /#\w+/i
+
+    body.gsub(hashtag_pattern) do |hashtag|
+      hashtag_without_hash = hashtag[1..-1]
+      link_to hashtag, search_path(query: hashtag_without_hash)
+    end
+  end
+
+  def avatar
+  end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
 
   has_many :followers, through: :follower_relationships
 
+  validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
